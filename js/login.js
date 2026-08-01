8753b8f02aa9ad41c8e2a9b6228f8c19f6ed4f0e
DiboraEyasuE <diboraeyasu792@gmail.com>
2026-08-01 20:40:20 +0300

    connected javascript functionally to the login page

--- js/login.js
+++ js/login.js
@@ -1,3 +1,13 @@
-const loginBtn = document.getElementsByClassName('login-btn');
-const nameInput = document.getElementById('name');
-cond
+const loginBtn = document.querySelector('.login-btn');
+
+loginBtn.addEventListener('click', showUser);
+
+
+loginBtn.addEventListener('click', showUser);
+
+function showUser(e) {
+    e.preventDefault(); // Stops the page from refreshing instantly
+    
+    const typedUsername = document.getElementById('name').value;
+    console.log('Username:', typedUsername);
+}
